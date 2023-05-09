6d388959c3ec64872124f5579cadd23bba17d85b
RuslanGSM <ruslangsm1553@gmail.com>
2023-05-09 20:45:57 +0300

    goit-js-hw-06

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -4,5 +4,4 @@ const nameOutput = document.getElementById('name-output');
 nameInput.addEventListener('input', function(event) {
     const name = event.target.value;
     nameOutput.textContent = name ? name : 'Anonymous';
-
 });
